b6f47a385d1027ad00ea8dd957d4a4109b4b4376
Idan Vork <ivork@f1r1s13.codam.nl>
2020-12-12 21:58:23 +0100

    fixed malloc error check and bonus headerfile

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,22 @@
 /*   By: anonymous <anonymous@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/26 17:27:02 by anonymous     #+#    #+#                 */
-/*   Updated: 2020/12/11 13:58:03 by ivork         ########   odam.nl         */
+/*   Updated: 2020/12/12 21:52:30 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include "get_next_line.h"
+
+int		ft_read(int fd, char *buff, int ret)
+{
+	ret = read(fd, buff, BUFFER_SIZE);
+	if (ret < 0)
+		return (-1);
+	buff[ret] = '\0';
+	return (ret);
+}
 
 int		get_next_line(int fd, char **line)
 {
@@ -23,17 +32,18 @@ int		get_next_line(int fd, char **line)
 		return (-1);
 	ret = 1;
 	*line = malloc(sizeof(char));
+	if (*line == NULL)
+		return (-1);
 	*line[0] = '\0';
 	while (ret)
 	{
 		if (!*buff)
-		{
-			ret = read(fd, buff, BUFFER_SIZE);
-			buff[ret] = '\0';
-			if (ret < 0)
-				return (-1);
-		}
+			ret = ft_read(fd, buff, ret);
+		if (ret < 0)
+			return (-1);
 		*line = ft_strcjoin(*line, buff);
+		if (*line == NULL)
+			return (-1);
 		if (ft_memmove(buff, buff + (ft_strchr(buff, '\n'))))
 			return (1);
 		ft_bzero(buff);
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,34 +6,44 @@
 /*   By: anonymous <anonymous@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/11 00:37:26 by anonymous     #+#    #+#                 */
-/*   Updated: 2020/12/11 13:55:10 by ivork         ########   odam.nl         */
+/*   Updated: 2020/12/12 21:51:10 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
+
+int		ft_read(int fd, char *buff, int ret)
+{
+	ret = read(fd, buff, BUFFER_SIZE);
+	if (ret < 0)
+		return (-1);
+	buff[fd][ret] = '\0';
+	return (ret);
+}
 
 int		get_next_line(int fd, char **line)
 {
 	static char	buff[1024][BUFFER_SIZE + 1];
 	int			ret;
 
-	if (BUFFER_SIZE <= 0)
+	if (BUFFER_SIZE <= 0 || fd < 0 || line == NULL)
 		return (-1);
 	ret = 1;
 	*line = malloc(sizeof(char));
+	if (*line == NULL)
+		return (-1);
 	*line[0] = '\0';
 	while (ret)
 	{
 		if (!*buff[fd])
-		{
-			ret = read(fd, buff[fd], BUFFER_SIZE);
-			buff[fd][ret] = '\0';
-			if (ret < 0)
-				return (-1);
-		}
+			ret = ft_read(fd, buff[fd], ret);
+		if (ret < 0)
+			return (-1);
 		*line = ft_strcjoin(*line, buff[fd]);
+		if (*line == NULL)
+			return (-1);
 		if (ft_memmove(buff[fd], buff[fd] + (ft_strchr(buff[fd], '\n'))))
 			return (1);
 		ft_bzero(buff[fd]);
