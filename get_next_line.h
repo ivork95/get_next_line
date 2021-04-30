55c03e87679b712bcac2af7159a3670cb1f19aa6
Idan Vork <ivork@f0r1s2.codam.nl>
2021-04-30 16:58:47 +0200

    modified header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/04 10:39:09 by anonymous     #+#    #+#                 */
-/*   Updated: 2020/12/11 12:21:21 by ivork         ########   odam.nl         */
+/*   Updated: 2021/04/30 16:55:49 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ int				ft_strchr(char *str, char c);
 char			*ft_strcjoin(char *s1, char *s2);
 char			*ft_memmove(char *dest, char *src);
 char			*ft_bzero(char *str);
-size_t			ft_strlcpy(char *dest, const char *src, size_t n);
 size_t			ft_strlen(const char *s);
 #endif
